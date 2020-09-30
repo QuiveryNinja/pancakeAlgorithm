bdc26910584dd38cce50242a404cae1eeaae1a7b
QuiveryNinja <64423006+QuiveryNinja@users.noreply.github.com>
2020-09-30 19:24:29 +0200

    Update pancakeSorting.py

--- pancakeSorting.py
+++ pancakeSorting.py
@@ -7,6 +7,9 @@ def SortPancakes():
     while i in pancakes:
         plate.append(pancakes[-1])
         pancakes.pop(-1)
+    for i in plate:
+        if i == 0:
+            plate.pop(i)
     
     return plate
 
